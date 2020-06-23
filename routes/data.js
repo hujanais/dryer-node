24a83f4ba31d81a2af4f40f8569595cab9ae383f
LEE Teik <tlee@europe.corp.altran.com>
2020-06-22 20:13:41 -0400

    gets cannot handle body

--- routes/data.js
+++ routes/data.js
@@ -4,10 +4,8 @@ const { envModel } = require('../models/data');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  const numOfPoints = req.body.numOfPoints;
-
-  var query = envModel.find({}, null, { sort: '-timestamp' }).limit(numOfPoints);
-  query.exec((err, docs) => {
+  const numOfPoints = parseInt(req.query.numOfPoints);
+  envModel.find({}, null, { sort: { timestamp: -1 }, limit: numOfPoints }, (err, docs) => {
     if (err) {
       res.send(err);
     } else {
